389a78d39ece5db663ec01a5e26b26f9b6294377
anjosdk <anjosddark@gmail.com>
2022-01-30 04:25:29 -0300

    okey

---
+++ Projeto1.js
@@ -0,0 +1,78 @@
+/*
+Você deve criar um programa que faça 5 perguntas para o usuário, e dependendo das respostas, vai exibir um resultado diferente no final. As perguntas devem ser todas de Sim ou Não, e devem ser referentes aos objetivos que ele tiveram que ser cumpridos durante a jornada, por exemplo: "Você conseguiu encontrar a Espada Mágica da Chama da Estrela do Inverno?", "Você conseguiu consertar os reatores nucleares do motor de dobra?", "Você se lembrou de passar no mercado e comprar óleo?"
+No final, o programa deve fazer uma contagem de quantas respostas foram Sim, e exibir o resultado de acordo com esse número:
+0 respostas Sim: Você falha miseravelmente.
+1 ou 2 respostas Sim: Você falha, mas ainda consegue fugir da situação.
+3 respostas Sim: Você chega perto de conseguir alcançar seu objetivo, mas acaba falhando por pouco.
+4 respostas Sim: Depois de muito esforço você conquista seu objetivo, embora não de maneira perfeita.
+5 respostas Sim: Você triunfa de maneira inquestionável e seus feitos serão lembrados por muitas gerações.
+O programa deve contar a história inicial (background), fazer as perguntas, e ao final, exibir o resultado de acordo com as situações acima.
+*/
+
+
+/*
+Era uma vez uma vez uma terra distante aonde existia magia e fora magia é claro existia também monstros e dragões e também demônios horripilantes nos qual tinham o objetivo de esterminar a raça humana de uma vez por todas...
+Nesse mundo existia um dragão lendário que ia até os céus e possuia a maior força de todas a maior velocidade entre todos os seres vivos no céu e alem disso as pele mais rigida de todas...
+Nossa história começa em um pequeno vilarejo no qual morava um jovem garoto que tinha um sonho de um dia virar um grande aventureiro ou até mesmo o heroi que derrotaria o rei demonio e salvaria toda a humanidade do perigo da extinsão e encontrar uma amizade verdadeira com alguém que ele levaria para o resto de sua vida.
+Um belo dia esse jovem ouviu dizer que os guerreiros mais fortes treinavam bastante até o ponto de serem o melhor em uma área e que começavam desde criança. Por exemplo um mago começava a estudar magias de livros desde criança os guerreiros mais fortes treinavam desde criança com espadas de madeira e lutas marciais para poderem quando precisarem manejar uma espada corretamente, assim como os grandes arqueiros desde pequenos focados em caçar para melhorar sua pontaria de todas as formas.
+
+1Sendo assim nosso jovem aventureiro sempre se deu bem com todas as pessoas do vilarejo, e sabendo do sonho que o menino tinha os melhores de seu condado se ofereceram para treina-lo no que ele precisa-se, ele deveria aceitar a oferta de seus anciões? S - N
+
+S- Ao aceitar treinar ele treinou todos os dias cada um com o melhor de sua função do vilarejo, um dia tinha aulas de magia, no outro de esgrima, no outro de arco e flecha, artes marciais e etc.
+N- Ao recusar treinar com os melhores nosso jovem aventureiro pensava que seria melhor não se esforçar tanto pois ele acreditava que na verdade para se tornar um grande aventureiro somente deveria ter os melhores itens que lhe trariam o melhor desempenho nas batalhas.
+
+O tempo passou e o aventureiro decidiu sair em sua primeira jornada na qual decidiu verificar qual seria o seu melhor destino, foi então que ele decidiu ir para a capital do seu empério e que de lá decidiria para onde iria em sua jornada...
+
+2 A caminho da capital ele encontrou uma caravana de pessoas que precisavam de ajuda contra um monstro grande e feroz no qual provavelmente todas aquelas pessoas morreriam sem ajuda. E então ele pensou se ajudaria aquela caravana de pessoas ou não? S - N
+
+S2-Ao decidir ajudar as pessoas ele por ser um dos aventureiros mais preparados do reino eliminou o monstro rápidamente e sem esforços salvou as pessoas da caravana, que ao chegarem na capital deram uma grande recompensa ao nosso aventureiro, que seria um cavalo que lhe ajudaria a se mover rapidamente para cumprir suas missões e o aventureiro ficou bastante feliz com a recompensa.
+
+1S-Ao decidir ajudar as pessoas por não ter treinado anteriormente e ter somente equipamentos básicos de aventureiros ele passou muita dificuldade para tentar lidar com a fera, porém acabou por conseguir derrota-la mesmo com a perca de algumas vidas durante o processo. E os sobreviventes da caravana lhe retribuiram com algum dinheiro que poderia lhe pagar o equivalente a uma noite em uma hospedagem na capital, de todo mal pelo menos ele não tera de dormir na rua em sua primeira visita a capital.
+
+N- Ao decidir não ajudar as pessoas da caravana nosso jovem aventureiro seguiu em frente e aproveitou que o monstro estava ocupado com as pessoas e fugiu para a cidade.
+
+Após os acontecimentos anteriores referente a caravana e a chegada na cidade, nosso aventureiro chegou até um lugar que seria chamado de guild no qual pessoas que precisam de 
+ajuda pedem auxilio a pessoas que estão dispostas a ajudar por uma certa recompensa, o trabalho perfeito para nosso aventureiro.
+
+3 Ao chegar lá ele parou para pensar se deveria se juntar a outras pessoas para poder ir atrás de recompensas maiores ou não, e deveria seguir o caminho do guerreiro solitário e ficar com toda a recompensa pra sí? S - N
+S3- Ele decide se juntar a algum grupo pois assim seria mais fácil de realizar missões dificeis e por conta de ser reconhecido como um dos guerreiros mais fortes e por conta de seu feito ao cuidar do monstro que atacou a caravana ele encontrou um grupo bem balanceado com pessoas bem fortes.
+S2- Ele decide se juntar a um grupo pois é mais facil de realizar missões mais díficeis.
+S1- ele decide se juntar ao grupo porém ninguém se interessa em se juntar a ele por ser uma pessoa totalmente dificil de lidar e não ter um bom treinamento, uma vez que se espalharam rumores de que ele viu o ataque contra a corroça e não fez nada.
+N - Realmente ficar com toda a recompensa para sí próprio provavelmente deve ser a melhor escolha uma vez que voce ficaria bem mais forte se tiver todo o dinheiro das recompensas para si.
+
+4 Logo então nosso aventureiro decide ir encontrar algum pedido no qual ele possa resolver e então chega a dona da guilda e lhe oferece uma missão de ir atrás de um dragão que se encontra nas montanhas nos arredores da capital e parece que o dragão está causando um alvoroço e atacando todos que passam por perto da estrada sendo que ele estava adomercido por anos e despertou após rumores de que o Rei dos demonios havia voltado.Nosso herói deve aceitar a missão de ir caçar o dragão? S - N
+S4- nosso aventureiro e seu grupo de amigos decidem ir atrás do dragão pois ele deve proteger a cidade de todos os males que acontecerem, uma vez que ele decidiu desde criança que seu objetivo era ser o heroi de seu povo.
+S3- nosso aventureiro aceita o desafio, mesmo sabendo que será dificil ele acredita que conseguirá vencer o dragão e conquistar a fama que merece. 
+S2-nosso aventureiro aceita o desafio, porém imagina que será incapaz de realizer seu objetivo, porém obsecado pela riquesa que teria ao derrotar um dragão e em toda a fama que isso lhe trará ela acaba aceitando
+S1-nosso aventureiro já sabe que não conseguirá derrotar o dragão, más só de ser sua primeira missão e ter aceitado todos em volta ficam surpresos com sua decisão.
+N-É claro existem aqueles que decidiriam não enfrentar o dragão, afinal seria uma luta totalmente imprudente e arriscar sua vida em uma aventura dessas seria loucura.
+
+N==
+5 Após alguns dias chega uma nova missão na qual o aventureiro deve se decidir entre explorar ou não uma masmorra nova que apareceu na cidade? S - N
+
+S4- Ao adentrar a masmorra ele consegue chegar em suas profundesas e achar uma espada rara que lhe daria poder o suficiente para poder estar dando sequencia em suas aventuras. E com o passar do tempo ele ficou conhecido por conta de todas as conquistas realizadas e foi chamado pelo herói para derrotar o rei demonio, e depois de muito trabalho conseguiu realizar sua conquista de derrotar o rei demonio com o heroi e ficar conhecido como um grande aventureiro que salvou a humanidade.FIM
+S3- Ao adentrar a masmorra o aventureiro passa por grandes desafios e não consegue se sair bem na exploração, e ao voltar a guild acaba se frustrando e dali em diante começou a pegar somente missões mais comuns e se tornou um aventureiro .FIM
+S==2||1- Ao adentrar a masmorra ele acaba sendo atacado por monstros e tem seus olhos feridos o tornando totalmente cego, e por conta disso ele acaba voltando para a sua cidade natal e vive uma vida comum de campones.FIM
+Ns0- Ao não escolher nada para fazer em suas jornadas voce vê que na verdade não era nada disso oque queria para sua vida e que ser campones não seria tão ruim assim, e ao voltar a sua cidade, descobrem que você viu a caravana ser atacada pelo monstro e não fez nada. Todos do seu vilarejo começaram a lhe despresar por isso e voce virou a vergonha da aldeia até chegar ao ponto de quase cometer suicidio.
+Ns3- Nosso aventureiro decide que na verdade só quer pegar missões mais faceis pois acredita que não tem um grande potencial, e acaba virando mais um aventureiro comum da cidade sem grandes espectativas em cima dele.FIM
+Ns>2||1- Nosso aventureiro decide que na verdade seria melhor voltar para sua aldeia e viver a vida de campones uma vez que ele acredita que as missões da cidade serião muito dificil para um ser comum como ele.FIM
+
+
+S==
+5.1 Então após partir para a missão, nosso aventureiro em sua batalha contra o dragão. Ele está quase derrotando o dragão e é quando o dragão fala com ele em sua mente e lhe pergunta o porque de ele estar invadino o seu território, porém o herói não dá ouvidos e continua os ataques até que ele está com a vida do dragão em suas mãos, e o dragão lhe pede que não lhe mate, pois sua furia foi calsada pela ascensão do rei demonio e quele deseja somente a queda do mesmo que seria na verdade o inimigo de nosso heroi, e então ele deve poupar a vida do dragão? S - N
+
+S5- nosso herói decide não matar o dragão e poupa sua vida, e eles acabam firmando um acordo até derrotarem o rei demonio. E após o passar dos anos com a ajuda de seus amigos sua melhor montaria,e agora do dragão o nosso herói consegue derrotar o rei demônio sem muito esforço, graças a todo preparo que ele teve anteriormente e toda ajuda que teve de seus amigos e do dragão que agora seria chamado de dragão lendário e o heroi ficou conhecido como o herói do dragão que acabaram se tornando grandes amigos durante a jornada, e sendo assim nosso herói conseguiu realizar seu sonho de se tornar o herói que derrotou o rei demonio. FIM
+
+S4- nosso aventureiro decide não matar o dragão e poupa sua vida, e os dois firmão um acordo e o dragão resolve voltar para sua hibernação uma vez que o heroi jurou que derrotaria o rei demonio, após algum tempo o aventureiro com muito esforço auxilia um herói que surgiu a derrotar o rei demonio e ficam conhecidos como os grandes aventureiros que ajudaram o heroi a derrotaram o rei demonio.FIM
+
+S3- nosso aventureiro decide não matar o dragão e poupa sua vida então o dragão foge e volta ao seu covil, e ao voltar a cidade e informar o acontecido, todos ficam felizes com a ausencia do dragão, porém o mesmo não recebe a recompensa pela missão, pois ele resolveu poupar a vida do dragão e sendo assim nosso aventureiro seguiu sua vida realizando missões e tendo uma vida bem comum como aventureiro.FIM
+
+S2- nosso aventureiro decide não matar o dragão e poupa sua vida porém na verdade se tratava tudo de uma armadilha do dragão e em seu momento de fúria incontrolável ele devora o braço de nosso aventureiro o deixando impossibilitado de usar sua espada, e então ao ver o aventureiro derrotado no chão o dragão retorna ao seu covil com um semblante de vitória. Logo então nosso aventureiro decide voltar a sua cidade já que havia perdido um braço e não conseguiria seguir seu sonho de ser um grande aventureiro ou ser o herói que salvou o mundo do rei demonio. FIM
+
+N S=4- nosso aventureiro decide por matar o dragão e ao retornar a cidade ele recebe a recompensa pela morte do dragão e se torna um grande aventureiro, no qual ficou conhecido por ter derrotado o dragão que estava destruindo a capital de seu pais, com o tempo um grande herói apareceu e nosso aventureiro se juntou a ele para conseguir derrotar o rei demonio e salvar a humanidade, e ficando assim conhecido como o grande aventureiro que ajudou na luta contra o Rei demonio.FIM
+
+N S=3 - nosso aventureiro decide por matar o dragão e ao retornar a cidade ele recebe uma recompensa grandiosa, porém ele decide por viver sua vida tranquilamente agora que tem bastante dinheiro devido a recompensa de sua primeira missão e decide abandonar sua vida de aventureiro e voltar para sua cidade de origem, uma vez que em sua primeira missão ele encontrou um grande desafio pode-se dizer que ele ficou com tralma de pegar outras missões mais dificeis.FIM
+
+N S=2 nosso aventureiro decide por matar o dragão e em sua tentativa acaba falhando e quase perdendo sua vida, porém consegue fugir da luta e voltar até a cidade. e foi ai que com o tralma que teve em sua primeira missão ele decide encontrar algum trabalho que não seja de aventureiro em sua cidade uma vez que ele não conseguira lutar contra monstros mais por conta de seu tralma da primeira missão.
+
+*/
